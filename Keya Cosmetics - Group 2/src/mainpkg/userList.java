06e605312abedd9a2cabc02c4cb7b7543e718842
HasinMahir77 <130781004+HasinMahir77@users.noreply.github.com>
2023-11-02 00:32:16 +0600

    UserList created

---
+++ Keya Cosmetics - Group 2/src/mainpkg/userList.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mainpkg;
+
+import HasinMahir.customer;
+import HasinMahir.customerServiceExecutive;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author hasin
+ */
+public class userList {
+    
+    //HasinMahir's Users
+    ArrayList<customer> customerList = new ArrayList<customer>();
+    ArrayList<customerServiceExecutive> customerServiceExecutiveList = new ArrayList<customerServiceExecutive>();
+    
+    //Borhan's Users
+    
+    
+    //Nadim's Users
+    
+    
+    //Amit's Users
+}
