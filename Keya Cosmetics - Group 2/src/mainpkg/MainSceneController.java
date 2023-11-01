3d7e6a49763ed67f9e6305afc842906c36f8651a
HasinMahir77 <130781004+HasinMahir77@users.noreply.github.com>
2023-11-01 22:36:37 +0600

    Testing the git

--- Keya Cosmetics - Group 2/src/mainpkg/MainSceneController.java
+++ Keya Cosmetics - Group 2/src/mainpkg/MainSceneController.java
@@ -18,12 +18,9 @@ import javafx.scene.control.Label;
 public class MainSceneController implements Initializable {
     
     @FXML
-    private Label label;
     
-    @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
-        label.setText("Hello World!");
     }
     
     @Override
