9285ee270de2bde9f66374f1fe193e935ed2d27c
BorhanGohan <iborhan36@gmail.com>
2023-11-13 19:23:07 +0600

    UserAC, UserDV created & UserList modified.

---
+++ Keya Cosmetics - Group 2/src/Borhan_accountant/BudgetingAndForecasting.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Borhan_accountant;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Masum
+ */
+public class BudgetingAndForecasting implements Serializable {
+    
+    private String item;
+    private float value;
+        
+    public BudgetingAndForecasting(String item,float value){
+        this.item = item;
+        this.value = value;
+    }
+        
+    public String getItem(){
+        return item;
+    }
+        
+    public float getValue(){
+        return value;
+    }
+        
+    public String setItem(String item){
+        this.item = item;
+        return item;
+    }
+        
+    public float setValue(float value){
+        this.value = value;
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "BudgetingAndForecasting{" + "item=" + item + ", value=" + value + '}';
+    }     
+}   
---
+++ Keya Cosmetics - Group 2/src/Borhan_accountant/FinancialRisk.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Borhan_accountant;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author MD Masum Billa
+ */
+public class FinancialRisk implements Serializable{
+    private String riskName;
+    private String description;
+    private double likelihood;
+    private double impact;
+
+    public FinancialRisk(String riskName, String description, double likelihood, double impact) {
+        this.riskName = riskName;
+        this.description = description;
+        this.likelihood = likelihood;
+        this.impact = impact;
+    }
+
+    public String getRiskName() {
+        return riskName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getLikelihood() {
+        return likelihood;
+    }
+
+    public double getImpact() {
+        return impact;
+    }
+    
+    @Override
+    public String toString() {
+        return "FinancialRisk{" + "riskName=" + riskName + ", description=" + description + ", likelihood=" + likelihood + ", impact=" + impact + '}';
+    }
+    
+}
