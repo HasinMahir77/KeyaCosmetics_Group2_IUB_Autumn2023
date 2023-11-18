55167c96bbb094a3c3dee9210acc5fc0f4f76b83
BorhanGohan <iborhan36@gmail.com>
2023-11-19 02:02:52 +0600

    made deliveryman fxml

--- Keya Cosmetics - Group 2/src/BorhanIslam/DeliverymanDashboardFXMLController.java
+++ Keya Cosmetics - Group 2/src/BorhanIslam/DeliverymanDashboardFXMLController.java
@@ -6,6 +6,8 @@ package BorhanIslam;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 /**
@@ -22,5 +24,41 @@ public class DeliverymanDashboardFXMLController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void profileOnButtonClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void CommsOnButtonClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void seeLocationOnButtonClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void confirmOnButtonClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void verifyCustomerIdOnButtonClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void checkAmountOnButtonClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void incomeOnButtonClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void logoutOnButtonClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void updateDeliveryOnButtonClicked(ActionEvent event) {
+    }
     
 }
