98e15b04ce063df04997ab4852d6cbe5e0f18bf9
HasinMahir77 <130781004+HasinMahir77@users.noreply.github.com>
2023-11-24 20:55:49 +0600

    DeliveryMan and Order WIP

--- Keya Cosmetics - Group 2/src/HasinMahir/Order.java
+++ Keya Cosmetics - Group 2/src/HasinMahir/Order.java
@@ -11,6 +11,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
+import mainpkg.ObjectOutputStreamA;
 
 /**
  *
@@ -24,51 +25,24 @@ public class Order implements Serializable {
     Cart cart;
     int id;
     
-    public Order(int id, String customer, String deliveryman) {
-        File idFile = new File("OrderID.bin");
-        if (idFile.exists()){
-            //Reading file
-            try(FileInputStream fis = new FileInputStream(idFile);
-                    ObjectInputStream ois = new ObjectInputStream(fis)){
-                this.id = (Integer)ois.readInt();
-            }
-            catch(Exception e){System.out.println(e.toString() + " from Order class");}
-            
-            //Deleting file
-            idFile.delete();
-            
-            //Rewriting file
-            try(FileOutputStream fos = new FileOutputStream(idFile);
-                    ObjectOutputStream oos = new ObjectOutputStream(fos)){
-                oos.writeInt(id+1);
-            }
-            catch(Exception e){System.out.println(e.toString() + " from Order class");}
-        }
-        else{
-            this.id=0;
-            //Writing file
-            try(FileOutputStream fos = new FileOutputStream(idFile);
-                    ObjectOutputStream oos = new ObjectOutputStream(fos)){
-                oos.writeInt(1);
-            }
-            catch(Exception e){System.out.println(e.toString() + " from Order class");}
-        }
-        this.id = id;
-        cart = new Cart(); 
+    public Order(String customer, Cart cart) {
+        this.cart = cart; 
         this.customerUserName = customer;
-        this.deliveryManUserName = deliveryman;
         this.status = Order.Status.PENDING;
     }
     
-    public Order(int id) {
+    public void generateId(){
+        
+        int id;
         File idFile = new File("OrderID.bin");
         if (idFile.exists()){
             //Reading file
             try(FileInputStream fis = new FileInputStream(idFile);
                     ObjectInputStream ois = new ObjectInputStream(fis)){
-                this.id = (Integer)ois.readInt();
+                id = (Integer)ois.readInt();
             }
-            catch(Exception e){System.out.println(e.toString() + " from Order class");}
+            catch(Exception e){
+                System.out.println(e.toString() + " from Order class");id=0;}
             
             //Deleting file
             idFile.delete();
@@ -80,19 +54,20 @@ public class Order implements Serializable {
             }
             catch(Exception e){System.out.println(e.toString() + " from Order class");}
         }
-        else{
-            this.id=0;
+        else{ //File doesn't exist. This is the first order.
+            id=0;
             //Writing file
             try(FileOutputStream fos = new FileOutputStream(idFile);
                     ObjectOutputStream oos = new ObjectOutputStream(fos)){
                 oos.writeInt(1);
             }
-            catch(Exception e){System.out.println(e.toString() + " from Order class");}
+            catch(Exception e){id=0;System.out.println(e.toString() + " from Order class");}
         }
         this.id = id;
-        cart = new Cart();
-        this.status = Order.Status.PENDING;
+        
     }
+    
+  
 
     public Status getStatus() {
         return status;
@@ -134,6 +109,73 @@ public class Order implements Serializable {
         this.id = id;
     }
     
+    public void saveInstance(){
+        File oldCustomerList = new File("OrderList.bin");
+        Order order;
+        ArrayList<Order> bufferList = new ArrayList<Order>();
+        //Collecting all the other users of same type except current user
+        try(FileInputStream fis = new FileInputStream(oldCustomerList);
+                ObjectInputStream ois = new ObjectInputStream(fis);) {
+            
+            while(true){
+                order = (Order)ois.readObject();
+                if ( !(order.getId()==this.getId()) ) {
+                    bufferList.add(order);
+                }
+            }
+        } catch(Exception e) {
+            System.out.println("From order.saveInstance() : "+e.toString());
+            System.out.println("ArrayList of orders made");
+        } 
+        // Arraylist of Orders made.
+        
+        //Rewriting the bin file with the updated customer object.
+       
+        try{
+            FileOutputStream temp = new FileOutputStream(oldCustomerList);
+            temp.close(); //Cleared the file
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        
+        //Writing files
+        try(FileOutputStream fos = new FileOutputStream(oldCustomerList);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);){
+            oos.writeObject(this);
+        } catch(Exception e){
+            System.out.println(e.toString());
+            System.out.println("Only current order written. Other customers next");
+        }
+        try(FileOutputStream fos = new FileOutputStream(oldCustomerList,true);
+        ObjectOutputStream oos = new ObjectOutputStreamA(fos);){
+            for(Order c: bufferList){
+                    oos.writeObject(c);
+                }
+        } catch(Exception e){
+            System.out.println(e.toString());
+        } 
+        
+        
+    }
     
+    public static ArrayList<Order> getOrderList(){
+        File oldCustomerList = new File("OrderList.bin");
+        Order order;
+        ArrayList<Order> customerList = new ArrayList<Order>();
+        //Collecting all the other users of same type
+        try(FileInputStream fis = new FileInputStream(oldCustomerList);
+                ObjectInputStream ois = new ObjectInputStream(fis);) {
+            
+            while(true){
+                order = (Order)ois.readObject();
+                customerList.add(order);
+            }
+        } catch(Exception e) {
+            System.out.println("From customer.getCustomerList() : "+e.toString());
+            System.out.println("ArrayList of customers made");
+        } 
+        // Arraylist of Orders made.
+        return customerList;
+    }
 
 }
