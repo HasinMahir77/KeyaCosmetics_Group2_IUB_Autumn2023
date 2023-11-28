c3ff0ae098731a656861b7282e92b81425ab7e5f
HasinMahir77 <130781004+HasinMahir77@users.noreply.github.com>
2023-11-29 00:48:04 +0600

    OrderCart added for DeliveryMan

--- Keya Cosmetics - Group 2/src/HasinMahir/deliveryManScenes/OrderCartController.java
+++ Keya Cosmetics - Group 2/src/HasinMahir/deliveryManScenes/OrderCartController.java
@@ -4,9 +4,22 @@
  */
 package HasinMahir.deliveryManScenes;
 
+import HasinMahir.Cart;
+import HasinMahir.Order;
+import HasinMahir.OrderedProduct;
+import HasinMahir.Product;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -15,12 +28,39 @@ import javafx.fxml.Initializable;
  */
 public class OrderCartController implements Initializable {
 
+    @FXML
+    private Label orderIdLabel;
+    @FXML
+    private TableColumn<OrderedProduct, String> nameColumn;
+    @FXML
+    private TableColumn<OrderedProduct, Integer> unitColumn;
+    @FXML
+    private TableColumn<OrderedProduct, Float> totalPriceColumn;
+
     /**
      * Initializes the controller class.
      */
+    ObservableList<Product> productArray = FXCollections.observableArrayList();
+    @FXML
+    private TableView<Product> cartTableView;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        Cart cart = null;
+        Stage stage = (Stage)cartTableView.getParent().getScene().getWindow();
+       
+        nameColumn.setCellValueFactory(new PropertyValueFactory<OrderedProduct, String>("name"));
+        unitColumn.setCellValueFactory(new PropertyValueFactory<OrderedProduct, Integer>("quantity"));
+        totalPriceColumn.setCellValueFactory(new PropertyValueFactory<OrderedProduct, Float>("totalPrice"));
+        
+        try{
+            cart = (Cart)stage.getUserData();
+        }
+        catch(Exception e){e.printStackTrace(System.out);}
+        
+        if (cart!=null){
+            cartTableView.getItems().addAll(cart.getProductOrderList());
+        }
     }    
     
 }
