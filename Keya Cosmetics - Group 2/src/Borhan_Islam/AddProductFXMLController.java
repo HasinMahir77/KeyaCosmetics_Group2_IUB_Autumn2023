ef106e0f57bd37dab1866100a07b11e92e3d574e
BorhanGohan <iborhan36@gmail.com>
2023-12-01 14:50:59 +0600

    1

--- Keya Cosmetics - Group 2/src/Borhan_Islam/AddProductFXMLController.java
+++ Keya Cosmetics - Group 2/src/Borhan_Islam/AddProductFXMLController.java
@@ -154,4 +154,8 @@ public class AddProductFXMLController implements Initializable {
         tableView.getItems().setAll(productArray);
         
     }    
+
+    @FXML
+    private void addProductNewListButton(ActionEvent event) {
+    }
 }
